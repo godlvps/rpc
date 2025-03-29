438cc616f218861cd1974721f0790d0b96df1cc5
godlvps <ameenop010@gmail.com>
2025-03-29 16:04:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ function reloadPresence(client) {
             .setDetails("HYDROX MUSIC")
             .setState("ENJOY MUSIC EFFORTLESSLY")
             .setStartTimestamp(global.startTime)
-            .setAssetsLargeImage("https://cdn.discordapp.com/attachments/1136290202118279270/1287750793084276776/VID-20240612-WA0013.gif")
-            .setAssetsLargeText("Listening to 127 servers")
+            .setAssetsLargeImage("https://cdn.discordapp.com/attachments/1136290202118279270/1316657869164380181/IMG-20240612-WA0012-2.jpg?ex=67e8e932&is=67e797b2&hm=a5d0bf8067d04b98b900c368bd1a3a6709b08f320caaddd2319865f34fc3f6b9&")
+            .setAssetsLargeText("Listening to 310 servers")
             .addButton('ADD TO SERVER', "https://discord.com/oauth2/authorize?client_id=1112673469210034256&permissions=36842105936&integration_type=0&scope=bot+applications.commands")
             .addButton('WEBSITE', "https://hydrox-music.rf.gd/"),
 
@@ -25,12 +25,12 @@ function reloadPresence(client) {
             .setType("WATCHING")
             .setName("HYDROX MULTI")
             .setDetails("Managing HYDROX MULTI")
-            .setState("Handling multiple accounts effortlessly")
+            .setState("Handling 16 Servers")
             .setStartTimestamp(global.startTime)
-            .setAssetsLargeImage("https://cdn.discordapp.com/attachments/1136290202118279270/1287750793084276776/VID-20240612-WA0013.gif")
+            .setAssetsLargeImage("https://cdn.discordapp.com/attachments/1136290202118279270/1355490158962806784/IMG-20241221-WA0067.jpg?ex=67e91e15&is=67e7cc95&hm=47b147657b94c79f1a286840cc3e37cc717ef60ae8375fc05cf264c56ecf36f3&")
             .setAssetsLargeText("Watching HYDROX MULTI")
-            .addButton('HYDROX MULTI', "https://hydrox.in/")
-            .addButton('JOIN DISCORD', "https://discord.gg/your-server-link")
+            .addButton('ADD TO SERVER', "https://discord.com/oauth2/authorize?client_id=1319551309757546496")
+            .addButton('INSTAGRAM', "https://www.instagram.com/hydrox.in?igsh=MXgwM3BzZTdkMDg4cA==")
     ];
 
     client.user.setPresence({
